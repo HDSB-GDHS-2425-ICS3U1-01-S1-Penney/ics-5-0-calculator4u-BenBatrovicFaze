0b1255166035c65022f1b07b586e67ef8c5e9aa0
BenBatrovicFaze <1toutrakaale@hdsb.ca>
2024-09-12 12:35:09 -0400

    Work In progress Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+let x = promt ("what is the X value?")
+let y = promt ("What is the Y value?")
+
+x = parseFloat(x)
+y = parseFloat(y)
+
+sum = x + y 
+difference = x - y
+product = x * y
+quotient = x/y
+
